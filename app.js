0c1436a852ef6b6f9a4f03230ee81d7ed60db88b
Mary Tosin Oladele <iamthedatagirl@gmail.com>
2023-11-27 08:00:52 +0100

    skill tree

---
+++ app.js
@@ -0,0 +1,9 @@
+
+const toggleDisplay = () => {
+    document.querySelector('.display').classList.toggle('hide')
+}
+document.addEventListener('keydown', evt => {
+    if (evt.key === 'Home') {
+        toggleDisplay();
+    }
+});
